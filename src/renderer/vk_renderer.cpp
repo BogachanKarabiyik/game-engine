7d7394fd0bcb46fb1cc9a6fb19bcc5e871dcbb3e
BogachanKarabiyik <bogacizmir@gmail.com>
2023-01-18 11:32:37 -0800

    Platform

---
+++ src/renderer/vk_renderer.cpp
@@ -0,0 +1,25 @@
+#include <vulkan/vulkan.h>
+
+struct VkContext{
+    VkInstance instance;
+};
+
+bool vk_init(VkContext* vkcontext)
+{
+    VkApplicationInfo appInfo = {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "Game";
+    appInfo.pEngineName = "GameEngine";
+
+    VkInstanceCreateInfo instanceInfo = {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    VkResult result = vkCreateInstance(&instanceInfo, 0, &vkcontext->instance);
+
+    if (result == VK_SUCCESS) {
+        return true;
+    }
+    
+    return false;
+}
